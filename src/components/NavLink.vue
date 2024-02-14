eb78358f889d1ad6fae8f566ba7b8ec8b6ef4869
Robert <robert.okenczyc@netblink.net>
2024-02-14 13:12:54 +0100

    Remove icons

--- src/components/NavLink.vue
+++ src/components/NavLink.vue
@@ -1,71 +1,9 @@
 <script setup>
 import { computed } from "vue";
 import { Link } from "@inertiajs/vue3";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import {
-    faChartSimple,
-    faUsers,
-    faMobileScreen,
-    faHandshake,
-    faDatabase,
-    faHardDrive,
-    faArrowRotateLeft,
-    faWifi,
-    faTruckField,
-    faTruckArrowRight,
-    faMapLocationDot,
-    faBarcode,
-    faWrench,
-    faWallet,
-    faWarehouse,
-    faMoneyBill1,
-    faReceipt,
-    faUsersRectangle,
-    faPeopleArrows,
-    faEnvelope,
-    faBell,
-    faBagShopping,
-    faCartArrowDown,
-    faUserGear,
-    faGavel,
-    faPlugCircleXmark,
-    faRepeat,
-} from "@fortawesome/free-solid-svg-icons";
-import { library } from "@fortawesome/fontawesome-svg-core";
-
-library.add(
-    faChartSimple,
-    faUsers,
-    faMobileScreen,
-    faHandshake,
-    faDatabase,
-    faHardDrive,
-    faArrowRotateLeft,
-    faWifi,
-    faTruckField,
-    faTruckArrowRight,
-    faMapLocationDot,
-    faBarcode,
-    faWrench,
-    faWallet,
-    faWarehouse,
-    faMoneyBill1,
-    faReceipt,
-    faUsersRectangle,
-    faPeopleArrows,
-    faEnvelope,
-    faBell,
-    faBagShopping,
-    faCartArrowDown,
-    faUserGear,
-    faGavel,
-    faPlugCircleXmark,
-    faRepeat
-);
 const props = defineProps({
     href: String,
     active: Boolean,
-    icon: String,
 });
 
 const classes = computed(() =>
@@ -73,12 +11,11 @@ const classes = computed(() =>
         ? "flex items-center p-2 text-base font-normal bg-accent text-gray-900 rounded-lg dark:text-white bg-gray-100 dark:hover:bg-gray-700"
         : "flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
 );
-const iconName = props.icon ? "fas " + props.icon : "";
 </script>
 
 <template>
     <Link :href="href" :class="classes">
-        <FontAwesomeIcon v-bind:icon="iconName" />
+        <slot name="icon" />
         <span class="ml-3 flex-1 whitespace-nowrap">
             <slot />
         </span>
