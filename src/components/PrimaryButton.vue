ae2c8c0b2685caa96c968c8b1a6096785dfcb7dc
eXway <r.okenczyc123@gmail.com>
2024-02-27 10:32:16 +0100

    Deploy from branch 1

--- src/components/PrimaryButton.vue
+++ src/components/PrimaryButton.vue
@@ -4,13 +4,15 @@ defineProps({
         type: String,
         default: "submit",
     },
+    disabled: Boolean,
 });
 </script>
 
 <template>
     <button
         :type="type"
-        class="focusable inline-flex items-center rounded-md border border-transparent bg-primary px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-primary-700 focus:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 active:bg-primary-700"
+        :disabled="disabled"
+        class="focusable inline-flex items-center rounded-md border border-transparent bg-primary px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-primary-700 focus:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 active:bg-primary-700 disabled:filter-gray disabled:pointer-events-none disabled:cursor-not-allowed"
     >
         <slot />
     </button>
--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,10 +1,12 @@
-<script setup>
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
-
-library.add(faSpinner);
-</script>
+<script setup></script>
 <template>
-    <FontAwesomeIcon class="fa-spin text-primary" icon="fa-spinner" />
+    <div
+        class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        role="status"
+    >
+        <span
+            class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+            >Loading...</span
+        >
+    </div>
 </template>
