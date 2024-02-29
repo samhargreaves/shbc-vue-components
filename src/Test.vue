170271269885f1aade3bed814f3727fdd307e898
eXway <r.okenczyc123@gmail.com>
2024-02-29 12:10:46 +0100

    Change styles

--- src/Test.vue
+++ src/Test.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { Input, Section } from "./index";
+import { Input, Section, RadioButton } from "./index";
 import { useForm } from "@inertiajs/vue3";
 
 const form = useForm({
@@ -7,6 +7,7 @@ const form = useForm({
     email: "",
     password: "",
     password_confirmation: "",
+    subscribe: false,
 });
 </script>
 <template>
@@ -15,5 +16,13 @@ const form = useForm({
         <Input v-bind:form="form" autofocus field="name" />
         <Input v-bind:form="form" field="email" type="email" />
         <Input v-bind:form="form" field="password" type="password" />
+        <p class="text-primary">Primary</p>
+        <p class="text-accent">Accent</p>
+        <RadioButton
+            value="eng_note"
+            v-model="form.subscribe"
+            name="option"
+            label="With Engineer Notes"
+        />
     </Section>
 </template>
