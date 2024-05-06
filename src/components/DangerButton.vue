51c66c9ca37947a5424c9f6cd86a43753313f056
Radosław Walczak <radoslawwalczak63@gmail.com>
2024-05-06 12:44:15 +0200

    Update DangerButton.vue

--- src/components/DangerButton.vue
+++ src/components/DangerButton.vue
@@ -15,7 +15,7 @@ defineProps({
     <button
         :type="type"
         :disabled="disabled"
-        class="focusable inline-flex items-center rounded border border-transparent bg-red-600 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 active:bg-red-700"
+        class="focusable inline-flex items-center rounded border border-transparent bg-red-600 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 active:bg-red-700 disabled:cursor-not-allowed disabled:brightness-125 disabled:grayscale-[30%]"
     >
         <slot />
     </button>
