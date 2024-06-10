35e58e6847768d102dbad30a0daf190aa5fe32d8
eXway <r.okenczyc123@gmail.com>
2024-06-10 15:08:12 +0200

    Update submit Button

--- src/components/Forms/SubmitButton.vue
+++ src/components/Forms/SubmitButton.vue
@@ -8,16 +8,17 @@ const props = defineProps({
     },
     disabled: Boolean,
     form: Object,
+    loading: { type: 'Boolean', default: false },
 });
 </script>
 
 <template>
-    <PrimaryButton :disabled="form.processing || disabled" :type="type" class="focusable">
-        <div :class="{ 'opacity-25': form.processing }">
+    <PrimaryButton :disabled="loading || form?.processing || disabled" :type="type" class="focusable">
+        <div :class="{ 'opacity-25': form?.processing || loading }">
             <slot />
         </div>
 
-        <div v-if="form.processing" class="absolute inset-2 flex items-center justify-center">
+        <div v-if="form?.processing || loading" class="absolute inset-2 flex items-center justify-center">
             <Spinner class="aspect-square !h-full !w-auto text-white"></Spinner>
         </div>
     </PrimaryButton>
