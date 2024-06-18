c9fa6e4408052081a22c5e516a257d997b1dd278
eXway <r.okenczyc123@gmail.com>
2024-06-18 12:16:46 +0200

    debug - Stats

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -7,6 +7,22 @@ const props = defineProps({
         type: String,
         default: null,
     },
+    customContainerClass: {
+        type: String,
+        default: '',
+    },
+    customStatClass: {
+        type: String,
+        default: '',
+    },
+    customStatLabelClass: {
+        type: String,
+        default: '',
+    },
+    customStatValueClass: {
+        type: String,
+        default: '',
+    },
 });
 
 const onClick = (status) => {
@@ -26,24 +42,25 @@ const ifSelected = (status) => {
 
 <template>
     <div>
-        <div class="flex flex-wrap gap-2 lg:flex-nowrap">
+        <div class="flex flex-wrap gap-2 lg:flex-nowrap" :class="{ [customContainerClass]: customContainerClass }">
             <div
                 v-for="s in stats"
                 class="flex w-full cursor-pointer flex-col rounded-lg border bg-gray-100 shadow"
                 :class="{
                     'border-primary bg-primary-50 shadow-primary-50': ifSelected(s.name),
+                    [customStatClass]: customStatClass,
                 }"
                 @click="onClick(s.name)"
                 :key="s.value"
             >
                 <div>
                     <div class="h:10 rounded-t-lg bg-white py-1 text-center md:h-10 md:py-2">
-                        <h5 class="text-xl font-medium leading-tight text-neutral-800">
+                        <h5 class="text-xl font-medium leading-tight text-neutral-800" :class="{ [customStatValueClass]: customStatValueClass }">
                             {{ s.value }}
                         </h5>
                     </div>
                 </div>
-                <div class="flex flex-auto flex-col gap-y-4 p-1 text-center md:py-2">
+                <div class="flex flex-auto flex-col gap-y-4 p-1 text-center md:py-2" :class="{ [customStatLabelClass]: customStatLabelClass }">
                     {{ s.label ?? s.name }}
                 </div>
             </div>
