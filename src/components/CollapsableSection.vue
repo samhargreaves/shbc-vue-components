379906b438d6878fbbbcc7b6324e3fbc322e161c
krzysztof <krzysztof.dutka@netblink.net>
2024-07-09 15:15:35 +0200

    added option for custom header color

--- src/components/CollapsableSection.vue
+++ src/components/CollapsableSection.vue
@@ -8,6 +8,9 @@ const props = defineProps({
         type: Boolean,
         default: false,
     },
+    headerColor: {
+        type: String,
+    },
 });
 
 const collapseRef = ref(null);
@@ -31,6 +34,7 @@ const onClick = () => {
             <div
                 @click="onClick"
                 class="focusable !block rounded-lg border-b-2 border-gray-200 bg-neutral-50 px-6 py-1 text-center"
+                :style="{ backgroundColor: headerColor + ' !important' }"
                 tabindex="0"
                 :aria-controls="header ? header.replaceAll(' ', '_') : `collapsable`"
             >
@@ -39,7 +43,7 @@ const onClick = () => {
                     {{ isVisible ? 'Hide' : 'Show' }}
                 </button>
             </div>
-            <div class="!visible hidden overflow-hidden" :id="header ? header.replaceAll(' ', '_') : `collapsable`" ref="collapseRef">
+            <div class="!visible hidden overflow-hidden bg-neutral-50" :id="header ? header.replaceAll(' ', '_') : `collapsable`" ref="collapseRef">
                 <div :class="{ 'px-4 pb-2': header }" class="mt-2">
                     <slot />
                 </div>
